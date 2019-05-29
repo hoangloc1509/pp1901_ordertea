bcf9ade32868e0a69a95ef4d7f9841e330e4bd81
hoangloc1509 <professorhoangloc@gmail.com>
2019-05-29 20:56:47 +0700

    init_project

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 8)->nullable(false)->change();
+            $table->string('fullname', 255)->nullable(false)->change();
+            $table->string('acc', 32)->nullable(false)->change();
+            $table->string('address', 255)->nullable(false)->change();
+            $table->integer('phone', 11)->nullable(false)->change();
+            $table->string('birth', 255)->nullable(false)->change();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}

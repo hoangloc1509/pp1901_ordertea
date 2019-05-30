da9e86fd474d337074c9bcd531e2b1bcae9c5183
hoangloc1509 <professorhoangloc@gmail.com>
2019-05-30 20:15:50 +0700

    init projct

---
+++ database/migrations/2019_05_30_122331_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255)->nullable(false);
+            $table->string('image', 255)->nullable(false);
+            $table->string('size', 32)->nullable(false);
+            $table->string('sugar', 32)->nullable(false);
+            $table->string('ice', 32)->nullable(false);
+            $table->integer('productprice');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
